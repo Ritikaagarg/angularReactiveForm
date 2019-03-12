fdf7c0ca2127b0137c8f890b3fb3a47ca54c857f
rgarg22 <rg22garg@outlook.com>
2019-03-12 18:39:32 +0530

    commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms'
+
+import { AppComponent } from './app.component';
+import { FormReactiveComponent } from './form-reactive/form-reactive.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FormReactiveComponent
+  ],
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/form-reactive/form-reactive.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-form-reactive',
+  templateUrl: './form-reactive.component.html',
+  styleUrls: ['./form-reactive.component.css']
+})
+export class FormReactiveComponent {
+  submitted:boolean = false;
+
+  profileForm = new FormGroup({
+    firstName: new FormControl('', Validators.required),
+    email: new FormControl('',Validators.email),
+    prefix: new FormControl('+91'),
+    PhoneNo: new FormControl('',[Validators.minLength(10),Validators.maxLength(10),Validators.pattern('[0-9]+')]),
+    address: new FormGroup({
+      street: new FormControl(''),
+      city: new FormControl(''),
+      state: new FormControl(''),
+      zip: new FormControl('',[Validators.maxLength(6),Validators.minLength(6),Validators.pattern('[0-9]+')])
+    })
+  })
+
+  get fcontrol(){
+    return this.profileForm.controls;
+  }
+
+  onSubmit() {
+    this.submitted=true;
+    
+    if (this.profileForm.invalid) {
+      return;
+  }
+
+  alert('SUCCESS!! :-)')
+  console.log(this.profileForm.value);
+  
+  }
+
+}
